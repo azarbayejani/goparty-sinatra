3cb7483afe167f03b60e494419596d1eb69e224d
Bobby Azarbayejani <bobby.azarbayejani@gmail.com>
2013-04-12 11:51:46 -0400

    BIG changes. I should have split this into multiple commits. Includes: email, front page, candidate pic upload

--- basics.rb
+++ basics.rb
@@ -1,11 +1,35 @@
 require 'sinatra'
 require 'data_mapper'
+require 'mandrill'
+require 'google_drive'
 #require "dm-migrations"
 #require 'digest/sha1'
 #require 'rack-flash3'
 #require 'sinatra-authentication'
 
 DataMapper::setup(:default,"sqlite3://#{Dir.pwd}/goparty.db")
+
+ENV['MANDRILL_APIKEY'] = "-2_pDSbUJL75U2hc5_0ilg"
+mandrill = Mandrill::API.new
+
+class String
+  def to_bool 
+    self == "true"
+  end
+end
+
+class Person
+  include DataMapper::Resource
+  
+  property :id, Serial
+  property :first_name, String
+  property :last_name, String
+  property :res_hall, String
+  property :college, String
+  property :email, String
+  property :phone, String
+
+end
 
 class Candidate
   include DataMapper::Resource
@@ -29,6 +53,7 @@ class Platform
   has n, :sections
 end
 
+
 class Section
   include DataMapper::Resource
 
@@ -37,19 +62,19 @@ class Section
   property :text, Text
   property :slug, String
 
-  has n, :problem
 
-  belongs_to :platform
+belongs_to :platform
 end
 
-class Problem
-  include DataMapper::Resource
+class Email 
+  include DataMapper::Resource 
 
   property :id, Serial
-  property :text, String, :required => true
-  property :solution, String, :required => true
+  property :title, String, :required => true
+  property :text, Text
+  property :sent, Boolean, :default => false
+  property :sent_time, DateTime
 
-  belongs_to :section
 end
 
 DataMapper.auto_upgrade!
@@ -63,7 +88,9 @@ helpers do
 end
 
 get '/' do
-  "Hello, world!"
+  erb :home do 
+    erb :init
+  end
 end
 
 get '/candidates' do
@@ -81,10 +108,23 @@ post '/candidates' do
   c[:description] = params[:description]
   c[:title]       = params[:title]
   c[:video]       = params[:video]
-  c[:img]         = params[:img]
   c[:type]        = params[:type]
-  c.save
-  redirect '/candidates'
+
+  if ( params[:img] && (tmpfile = params[:img][:tempfile]) &&
+       (name = params[:img][:filename]) )
+
+
+    File.open(File.join(Dir.pwd,"public/uploads",name),"wb") {
+      |f| f.write(tmpfile.read)
+    }
+
+    c[:img] = "/uploads/" + name
+
+    c.save
+    redirect '/candidates'
+  else
+    redirect '/candidates/create'
+  end
 end
 
 get '/candidates/create' do
@@ -118,3 +158,108 @@ post '/platform' do
   p.save
   redirect '/platform' 
 end
+
+get '/email/create' do
+  erb :email_form
+end
+
+get '/email' do
+  @sent_emails = Email.all( :sent => true )
+  @unsent_emails = Email.all( :sent => false, :order=> :sent_time.asc)
+  erb :email
+end
+
+post '/email' do
+
+  e = Email.new
+  e[:title] = params[:subject]
+  e[:text] = params[:body]
+  e[:sent] = false
+  e.save
+
+  redirect '/email'
+
+end
+
+post '/email/:id' do
+  e = Email.get( params[:id].to_i )
+  e[:sent] = params[:send].to_bool
+  e[:sent_time] = Time.now
+  e.save
+
+  #GOOGLE API STUFF
+  recipients = [ 
+    {
+      :email => "bobby.azarbayejani@gmail.com",
+      :name => "Bobby Azarbayejani" 
+    },
+    { 
+      :email => "aashish.gadani@gmail.com",
+      :name => "Aashish Gadani"
+    }
+  ] 
+
+  ## MANDRILL API SEND
+  mandrill.messages().send({
+    :html => e[:text],
+    :subject => e[:title],
+    :from_email => "info@gopartyumd.com",
+    :from_name => "Go Party",
+    :to => recipients,
+    :track_opens => false,
+    :track_clicks => false,
+    :auto_text => true,
+    :auto_html => true,
+    :inline_css => true
+  })
+
+  redirect '/email'
+
+end
+
+#survey has cols in this order:
+# [ timestamp , first , last , college , reshall , email , phone ]
+post '/survey' do
+
+  p = Person.new
+  p[:first_name] = params[:first]
+  p[:last_name] = params[:last]
+  p[:email] = params[:email]
+  p.save
+
+  redirect "/completesurvey?person_id=#{p[:id]}"
+
+end
+
+get '/completesurvey' do
+
+  unless request.referrer && request.referrer.include?(request.host)
+    redirect '/'
+  end
+
+  erb :home do 
+    @person = Person.get(params["person_id"].to_i)
+    erb :more_info
+  end  
+
+end
+
+post '/completesurvey' do
+  
+  p = Person.get(params[:person_id].to_i)
+  p[:res_hall] = params[:residence_hall]
+  p[:phone] = params[:phone]
+  p[:college] = params[:college]
+
+  p.save
+
+  redirect '/thanks'
+
+end
+
+get '/thanks' do
+  erb :home do
+    erb :thanks
+  end
+end
+
